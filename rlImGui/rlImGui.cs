f292152c4577f5d673aebe692de72df55cb08b61
Jeff Myers <JeffM2501@gmail.com>
2024-02-02 21:26:17 -0800

    only fullscreen uses monitor size, otherwise we are off by the titlebar.

--- rlImGui/rlImGui.cs
+++ rlImGui/rlImGui.cs
@@ -343,7 +343,7 @@ namespace rlImGui_cs
         {
             ImGuiIOPtr io = ImGui.GetIO();
 
-            if (Raylib.IsWindowFullscreen() || Raylib.IsWindowMaximized())
+            if (Raylib.IsWindowFullscreen())
             {
                 int monitor = Raylib.GetCurrentMonitor();
                 io.DisplaySize = new Vector2(Raylib.GetMonitorWidth(monitor), Raylib.GetMonitorHeight(monitor));
